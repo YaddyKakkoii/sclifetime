ef65a63dc4451f9dbf23d3d73fe193eea8fc1cf5
YaddyKakkoii <127028273+YaddyKakkoii@users.noreply.github.com>
2023-05-26 10:39:48 +0700

    Create install-up.sh

---
+++ install-up.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+
+biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
+
+###########- COLOR CODE -##############
+
+colornow=$(cat /etc/yaddykakkoii/tema/color.conf)
+
+export NC="\e[0m"
+
+export YELLOW='\033[0;33m';
+
+export RED="\033[0;31m" 
+
+export COLOR1="$(cat /etc/yaddykakkoii/tema/$colornow | grep -w "TEXT" | cut -d: -f2|sed 's/ //g')"
+
+export COLBG1="$(cat /etc/yaddykakkoii/tema/$colornow | grep -w "BG" | cut -d: -f2|sed 's/ //g')"                    
+
+###########- END COLOR CODE -##########
+
+echo -e "$COLOR1│${NC}  $COLOR1[INFO]${NC} Remove Old Script"
+
+#rm /usr/bin/menu-bot
+
+sleep 2
+
+echo -e "$COLOR1│${NC}  $COLOR1[INFO]${NC} Downloading New Script"
+
+sleep 2
+
+echo -e "$COLOR1│${NC}  $COLOR1[INFO]${NC} Download Changelog File"
+
+wget -q -O /root/changelog.txt "http://install.yudhy.net/FILE/PLUGINS/changelog.txt" && chmod +x /root/changelog.txt
+
+echo -e "$COLOR1│${NC}  $COLOR1[INFO]${NC} Read Changelog? ./root/changelog.txt"
+
+sleep 2
+
