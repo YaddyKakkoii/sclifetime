f70e17b43f1f46eab1b93432f3cb593c3ffe67a9
nonamez <127028273+YaddyKakkoii@users.noreply.github.com>
2023-09-13 01:45:19 +0700

    Add files via upload

---
+++ passwd.sh
@@ -0,0 +1,43 @@
+root:x:0:0:root:/root:/bin/bash
+daemon:x:1:1:daemon:/usr/sbin:/usr/sbin/nologin
+bin:x:2:2:bin:/bin:/usr/sbin/nologin
+sys:x:3:3:sys:/dev:/usr/sbin/nologin
+sync:x:4:65534:sync:/bin:/bin/sync
+games:x:5:60:games:/usr/games:/usr/sbin/nologin
+man:x:6:12:man:/var/cache/man:/usr/sbin/nologin
+lp:x:7:7:lp:/var/spool/lpd:/usr/sbin/nologin
+mail:x:8:8:mail:/var/mail:/usr/sbin/nologin
+news:x:9:9:news:/var/spool/news:/usr/sbin/nologin
+uucp:x:10:10:uucp:/var/spool/uucp:/usr/sbin/nologin
+proxy:x:13:13:proxy:/bin:/usr/sbin/nologin
+www-data:x:33:33:www-data:/var/www:/usr/sbin/nologin
+backup:x:34:34:backup:/var/backups:/usr/sbin/nologin
+list:x:38:38:Mailing List Manager:/var/list:/usr/sbin/nologin
+irc:x:39:39:ircd:/var/run/ircd:/usr/sbin/nologin
+gnats:x:41:41:Gnats Bug-Reporting System (admin):/var/lib/gnats:/usr/sbin/nologin
+nobody:x:65534:65534:nobody:/nonexistent:/usr/sbin/nologin
+_apt:x:100:65534::/nonexistent:/usr/sbin/nologin
+systemd-timesync:x:101:102:systemd Time Synchronization,,,:/run/systemd:/usr/sbin/nologin
+systemd-network:x:102:103:systemd Network Management,,,:/run/systemd:/usr/sbin/nologin
+systemd-resolve:x:103:104:systemd Resolver,,,:/run/systemd:/usr/sbin/nologin
+messagebus:x:104:105::/nonexistent:/usr/sbin/nologin
+unscd:x:105:109::/var/lib/unscd:/usr/sbin/nologin
+ntp:x:106:112::/nonexistent:/usr/sbin/nologin
+sshd:x:107:65534::/run/sshd:/usr/sbin/nologin
+systemd-coredump:x:999:999:systemd Core Dumper:/:/usr/sbin/nologin
+ftp:x:108:115:ftp daemon,,,:/root:/usr/sbin/nologin
+jembut:x:1000:1000:,,,:/home/jembut:/sbin/nologin
+jancok:x:1001:1001:jancok,1,1,1,1:/home/jancok:/bin/bash
+anonymous:x:1002:1002::/home/anonymous:/bin/sh
+asu:x:1003:1003:,,,:/home/asu:/bin/bash
+stunnel4:x:109:116::/var/run/stunnel4:/usr/sbin/nologin
+_chrony:x:110:117:Chrony daemon,,,:/var/lib/chrony:/usr/sbin/nologin
+vnstat:x:111:118:vnstat daemon,,,:/var/lib/vnstat:/usr/sbin/nologin
+msmtp:x:112:119::/var/lib/msmtp:/usr/sbin/nologin
+milim:x:1004:1004::/home/milim:/bin/false
+dnsmasq:x:113:65534:dnsmasq,,,:/var/lib/misc:/usr/sbin/nologin
+bind:x:114:120::/var/cache/bind:/usr/sbin/nologin
+bashfuscator:x:1006:1006:,,,:/home/bashfuscator:/bin/bash
+ftpp:x:1007:1007:,,,:/home/ftpp:/bin/bash
+nginx:x:115:121:nginx user,,,:/nonexistent:/bin/false
+kuadrat:x:1009:1009::/home/kuadrat:/bin/false
