3c8ec138d7557dbe4eddc6f8f92baa61dd97672e
nonamez <127028273+YaddyKakkoii@users.noreply.github.com>
2023-10-22 17:23:27 +0700

    Create downloadmenu.sh

---
+++ downloadmenu.sh
@@ -0,0 +1,9 @@
+REPO="https://raw.githubusercontent.com/YaddyKakkoii/sclifetime/main/"
+
+wget ${REPO}menu.zip
+unzip menu.zip
+chmod +x menu/*
+#mv menu/* /usr/local/sbin
+mv menu/* /usr/bin
+rm -rf menu
+rm -rf menu.zip
