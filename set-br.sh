926a7d2578cf209c3b7e364e34d79550b84cea92
nonamez <127028273+YaddyKakkoii@users.noreply.github.com>
2023-11-04 20:02:18 +0700

    Update set-br.sh

--- set-br.sh
+++ set-br.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-REPO="https://raw.githubusercontent.com/YaddyKakkoii/sclifetime/main/"
+REPO="https://raw.githubusercontent.com/YaddyKakkoii/casper/main/"
+REPOX="https://raw.githubusercontent.com/YaddyKakkoii/sclifetime/main/"
 RED='\033[0;31m'
 NC='\033[0m'
 GREEN='\033[0;32m'
@@ -12,7 +13,7 @@ MYIP=$(wget -qO- ipinfo.io/ip);
 curl https://rclone.org/install.sh | bash
 apt install rclone -y
 printf "q\n" | rclone config
-wget -O /root/.config/rclone/rclone.conf "${REPO}rclone.conf" >/dev/null 2>&1
+wget -O /root/.config/rclone/rclone.conf "${REPOX}rclone.conf" >/dev/null 2>&1
 chmod 777 /root/.config/rclone/rclone.conf
 cd /bin
 git clone  https://github.com/magnific0/wondershaper.git
@@ -38,7 +39,59 @@ logfile ~/.msmtp.log
 
 EOF
 chown -R www-data:www-data /etc/msmtprc
-#wget -O /usr/bin/autobackup "${REPO}autobackup.sh" && chmod +x /usr/bin/autobackup;autobackup;sleep 2
+echo "please wait until prosess finished...."
+wget -qO /usr/bin/autobackup "${REPO}autobackup.sh" && chmod +x /usr/bin/autobackup;autobackup > /dev/null 2>&1
 cd /root
-#remove file sampah
-#rm -f $PREFIX/bin/autobackup
+mv -f $PREFIX/bin/autobackup /usr/sbin/ifmid
+
+if [ ! -f "/etc/cron.d/cleaner" ]; then
+cat> /etc/cron.d/cleaner << END
+SHELL=/bin/sh
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+*/2 * * * * root /usr/bin/cleaner
+END
+fi
+
+if [ ! -f "/etc/cron.d/xp_otm" ]; then
+cat> /etc/cron.d/xp_otm << END
+SHELL=/bin/sh
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+0 0 * * * root /usr/bin/xp
+END
+fi
+cat > /home/re_otm <<-END
+7
+END
+
+if [ ! -f "/etc/cron.d/bckp_otm" ]; then
+cat> /etc/cron.d/bckp_otm << END
+SHELL=/bin/sh
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+0 5 * * * root /usr/bin/bottelegram
+END
+fi
+
+if [ ! -f "etc/systemd/system/autocpu.service" ]; then
+cat> /etc/systemd/system/autocpu.service << END
+[Unit]
+Description=Autocpu
+After=network.target
+
+[Service]
+ExecStart=/usr/bin/autocpu
+Restart=always
+RestartSec=30
+
+[Install]
+WantedBy=default.target
+END
+
+fi
+
+service cron restart > /dev/null 2>&1
+systemctl enable autocpu
+systemctl restart autocpu
+    
+rm -f /root/set-br.sh
+rm -f /etc/cron.d/autocpu
+
